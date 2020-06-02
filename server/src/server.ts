76ed5b0c6e1d8dc793f4c17e675d53af1145eba6
Maicon Andraski <maiconm23@gmail.com>
2020-06-02 10:37:46 -0300

    🛡 Routes and database setup
    - installed knexks;
    - installed sqlite;
    - database connection;
    - created routes file;
    - removed sample routes;

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,45 +1,9 @@
 import express from 'express';
-
+import routes from './routes'
 const app = express();
 
 app.use(express.json());
 
-const users = [
-  'Diego',
-  'Cleiton',
-  'Robson',
-  'Jhons'
-];
-
-app.get('/', (request, response) => {
-  const hello ='hello world';
-  console.log(hello);
-  response.send(hello);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = +request.params.id;
-
-  const user = users[id];
-
-  response.json(user);
-});
-
-app.get('/users', (request, response) => {
-  const search = request.query.search;
-  console.log('xxx', search)
-  const filteredUsers = search ? users.filter(user => user.includes(`${search}`)) : users
-  response.json(filteredUsers);
-});
-
-app.post('/users', (request, response) => {
-  const { name, email } = request.body
-  const user = {
-    name,
-    email
-  };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
