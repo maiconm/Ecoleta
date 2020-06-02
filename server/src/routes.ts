2a8c9e1625d4a007866f949d796e2113d1f0dd96
Maicon Andraski <maiconm23@gmail.com>
2020-06-02 11:48:31 -0300

    ⛏ List items
    - added SVGs;
    - created route to list items;
    - script to ignore changes in node_modules;
    - deleted sample endpoints in http file;
    - created static route to serve images;
    - added flag to remove warning in terminal (`connectin.ts`);
    - created seed to populate items table;

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  response.json({ message: 'Hello World' });
+routes.get('/items', async (request, response) => {
+  const items = await <Promise<any>>knex('items').select('*');
+  
+  const serializedItems = items.map(
+    ({title, image}: {title: string, image: string}) => (
+      {title, image_url: `http://localhost:3333/uploads/${image}`}
+    )
+  );
+
+  response.json(serializedItems);
 });
 
 export default routes;
