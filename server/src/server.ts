08b623b5980c09ca64b1edb990d72402c557c22d
Maicon Andraski <maiconm23@gmail.com>
2020-06-02 09:53:41 -0300

    🚁 Concepts
    - HTTP methods;
    - Params types;
    - Routes and resources;

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,44 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+  'Diego',
+  'Cleiton',
+  'Robson',
+  'Jhons'
+];
+
+app.get('/', (request, response) => {
+  const hello ='hello world';
+  console.log(hello);
+  response.send(hello);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = +request.params.id;
+
+  const user = users[id];
+
+  response.json(user);
+});
+
 app.get('/users', (request, response) => {
-  console.log('hello world');
-  response.send([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Jhons'
-  ]);
+  const search = request.query.search;
+  console.log('xxx', search)
+  const filteredUsers = search ? users.filter(user => user.includes(`${search}`)) : users
+  response.json(filteredUsers);
+});
+
+app.post('/users', (request, response) => {
+  const { name, email } = request.body
+  const user = {
+    name,
+    email
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
